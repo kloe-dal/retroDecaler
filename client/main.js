1529f3e714968b1bdeb24879911840fa2ae6422d
Pntbr <pntbr@scopyleft.fr>
2014-07-22 01:13:51 +0200

    Initialize landing page

---
+++ client/main.js
@@ -0,0 +1,8 @@
+Template.subscribe.events({
+  'submit form': function (event) {
+    event.preventDefault();
+    var email = $('form input').val();
+    Email.insert({email: email, created_at: new Date()});
+    $('form input').val("");
+  }
+});
---
+++ collections/collections.js
@@ -0,0 +1,1 @@
+Email = new Meteor.Collection("email");
